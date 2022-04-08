52c72075687e611307d86f2af573575c6776f33b
MartinMaciel110916 <martinmacielbramoso@gmail.com>
2022-04-08 15:17:46 -0300

    Tarea

---
+++ Clase4 Prog3/js/js-externo.js
@@ -0,0 +1,3 @@
+document.getElementById("parrafo1").style.color = "red";
+       document.getElementById("parrafo2").style.color = "blue";
+       document.getElementById("parrafo3").style.color = "green";
